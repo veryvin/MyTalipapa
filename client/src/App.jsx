d738c03d5ff47ccfb761b77b95816ee8f7370d1b
veryvin <earvinjohnlopez01@gmail.com>
2026-05-22 18:22:29 +0800

    dasdsa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
